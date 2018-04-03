18eb6ab9e5ef4ffe56ef9a886452bfc7df8a1403
redcatbox@gmail.com <RedCatBox_0123>
2018-04-03 16:32:54 +0300

    started junit

---
+++ src/test/java/calculator/CalculatorTests.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import home.dbarannik.Calculator.Calculator;
+import org.junit.Before;
+
+public class CalculatorTests {
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+}
